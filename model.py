3df73c76af92273ced0cad1c07fb6c13681ecd81
dinupa1 <dinupa3@gmail.com>
2023-01-21 22:13:32 -0600

    xavier normal weights refer dimuDNN-01-21-2023.pdf

--- model.py
+++ model.py
@@ -5,15 +5,21 @@ from torch.nn import Linear
 class Tagger(torch.nn.Module):
     def __init__(self, in_features=26, out_features=5):
         super(Tagger, self).__init__()
-        self.fc1 = torch.nn.Linear(in_features, 50, bias=True)
-        self.fc2 = torch.nn.Linear(50, 50, bias=True)
-        self.fc3 = torch.nn.Linear(50, 30, bias=True)
-        self.fc4 = torch.nn.Linear(30, out_features, bias=True)
+        self.fc1 = torch.nn.Linear(in_features, 124, bias=True)
+        torch.nn.init.xavier_normal_(self.fc1.weight)
+        self.fc2 = torch.nn.Linear(124, 124, bias=True)
+        torch.nn.init.xavier_normal_(self.fc2.weight)
+        self.fc3 = torch.nn.Linear(124, 124, bias=True)
+        torch.nn.init.xavier_normal_(self.fc3.weight)
+        # self.dropout = torch.nn.Dropout(0.1)
+        self.fc4 = torch.nn.Linear(124, out_features, bias=True)
+        torch.nn.init.xavier_normal_(self.fc4.weight)
         
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
+        # x = self.dropout(x)
         x = F.relu(self.fc4(x))
         return x
     
@@ -21,37 +27,46 @@ class Tagger(torch.nn.Module):
 class Regressor(torch.nn.Module):
     def __init__(self, in_features=31, out_features=7):
         super(Regressor, self).__init__()
-        self.fc1 = torch.nn.Linear(in_features, 50, bias=True)
-        self.fc2 = torch.nn.Linear(50, 50, bias=True)
-        self.fc3 = torch.nn.Linear(50, 20, bias=True)
-        self.fc4 = torch.nn.Linear(20, out_features, bias=True)
+        self.fc1 = torch.nn.Linear(in_features, 248, bias=True)
+        torch.nn.init.xavier_normal_(self.fc1.weight)
+        # self.norm1 = torch.nn.BatchNorm1d(50)
+        self.fc2 = torch.nn.Linear(248, 248, bias=True)
+        torch.nn.init.xavier_normal_(self.fc2.weight)
+        self.fc3 = torch.nn.Linear(248, 248, bias=True)
+        torch.nn.init.xavier_normal_(self.fc3.weight)
+        self.fc4 = torch.nn.Linear(248, 248, bias=True)
+        torch.nn.init.xavier_normal_(self.fc4.weight)
+        self.fc5 = torch.nn.Linear(248, out_features, bias=True)
+        torch.nn.init.xavier_normal_(self.fc5.weight)
         
     def forward(self, x):
         x = F.relu(self.fc1(x))
+        # x = self.norm1(x)
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = F.relu(self.fc4(x))
+        x = self.fc5(x)
         return x
     
 
     
-class LeNet5Tagger(torch.nn.Module):
+class ConvTagger(torch.nn.Module):
     def __init__(self):
-        super(LeNet5Tagger, self).__init__()
-        self.conv1 = torch.nn.Conv2d(1, 6, 5, padding=2)
-        self.pool1 = torch.nn.AvgPool2d(2, 2)
-        self.conv2 = torch.nn.Conv2d(6, 16, 5)
-        self.pool2 = torch.nn.AvgPool2d(2, 2)
-        self.fc1 = torch.nn.Linear(16*4*4, 120)
+        super(ConvTagger, self).__init__()
+        self.conv1 = torch.nn.Conv1d(1, 10, 5, padding=2)
+        self.pool1 = torch.nn.AvgPool1d(2)
+        self.conv2 = torch.nn.Conv2d(10, 20, 5)
+        self.pool2 = torch.nn.AvgPool1d(2)
+        self.fc1 = torch.nn.Linear(20* 10, 120)
         self.fc2 = torch.nn.Linear(120, 5)
 
     def forward(self, x):
-        x = x.unsqueeze(0).unsqueeze(0)
+        x = x.unsqueeze(0)
         x = F.relu(self.conv1(x))
         x = self.pool1(x)
         x = F.relu(self.conv2(x))
         x = self.pool2(x)
-        x = x.view(-1, 16*4*4)
+        x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.softmax(x, dim=-1)
